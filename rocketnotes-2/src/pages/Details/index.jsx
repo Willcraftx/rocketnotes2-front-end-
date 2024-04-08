7ba2a77a724681472340f173aa03579be4d5bac9
William.Y <will_jpn@yahoo.com.br>
2024-04-08 20:56:02 +0900

    Start

---
+++ rocketnotes-2/src/pages/Details/index.jsx
@@ -0,0 +1,50 @@
+import { Container, Links, Content } from "./styles";
+
+import { Tag } from "../../components/Tag";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
+
+export function Details() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quos tempore labore, repellat culpa placeat iure neque repudiandae.
+            Quasi, omnis a! Dolorum libero amet animi voluptate quos nam architecto
+            sint totam.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
+    </Container>
+  );
+}
